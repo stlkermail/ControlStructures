a75cee1e7a8812555329735c99ef026844d4637f
Repti <Repti@BOX>
2023-02-24 18:23:03 +0500

    Add project files.

---
+++ ControlStructures/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	cout << "Hello Controls!\n" << endl;
+
+	int t;
+	cout << "Введите температуру воздуха: \n\n"; cin >> t; cout << "\n";
+
+	if (t > 0)
+	{
+		cout << "На улице тепло " << endl;
+	}
+	else
+	{
+		cout << "\nНа улице холодно " << endl;
+	}
+
+}
